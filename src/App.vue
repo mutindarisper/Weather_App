08a7783eeeff28f77023843eacd449af21459e23
unknown <mutindarisper@gmail.com>
2022-05-11 12:46:31 +0300

    first weather app commit

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,14 @@ export default {
 }
 </script>
 
-<style>
-#app {
+<style scoped>
+@import "./assets/app.css";
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
