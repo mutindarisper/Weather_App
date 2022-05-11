4afed5c6b86ccc71646e5041eef34686599e3463
unknown <mutindarisper@gmail.com>
2022-05-11 16:59:48 +0300

    implemented icons to change according to weather

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 20 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input type="text" name="" id="" class="search-bar"
@@ -8,7 +8,7 @@
          @keypress="fetchWeather">
       </div>
 
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined' ">
+     <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}},  {{weather.sys.country}}</div>
           <div class="date">{{ pickCurrentDate()}}</div>
@@ -17,9 +17,14 @@
 
         <div class="weather-box">
          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
-         <div class="weather">{{weather.weather[0].main}}</div>
-
+         <div class="weather">{{ weather.weather[0].main }}</div>
+         <div icon="sunny" class="sun-icon" v-if="sunny"> </div>
+          <div icon="cloudy" class="cloud-icon" v-if="cloudy"> </div>
+           <div icon="rainy" class="rain-icon" v-if="rainy"> </div>
+       
+          
         </div>
+
       </div>
     </main>
     
@@ -36,7 +41,10 @@ export default {
       api_key: '60938220bb9fc41c7b5444ae8ebbfc09',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
-      weather: {}
+      weather: {},
+      sunny: false,
+      cloudy: false,
+      rainy: false,
     }
   },
   components: {
@@ -54,7 +62,27 @@ export default {
 
     },
     setResults(results) {
+      
       this.weather = results
+      if(this.weather.weather[0].main.includes('Clouds')) {
+        this.cloudy = true;
+        this.sunny = false;
+        this.rainy= false;
+
+      }
+      if(this.weather.weather[0].main.includes('Rain')) {
+        this.cloudy = false;
+        this.sunny = false;
+        this.rainy= true;
+
+      }
+      if(this.weather.weather[0].main.includes('Clear')) {
+        this.cloudy = false;
+        this.sunny = true;
+        this.rainy= false;
+
+      }
+     
     },
     pickCurrentDate() {
       let d = new Date();
